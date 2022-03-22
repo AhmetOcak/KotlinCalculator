486493cb6338961460468de684a414918b11d576
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2022-03-22 16:14:04 +0300

    equal ops finished

--- app/src/main/java/com/kotlincalculator/MainActivity.kt
+++ app/src/main/java/com/kotlincalculator/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var canAddDat = true
     private var canConclude = true
+    private var operationList = mutableListOf<String>()
+    private var digitsList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    private fun controlSpecialCharacters() : Boolean {
+    private fun controlSpecialCharacters(): Boolean {
         return when {
             binding.process.text.last() == '.' -> false
             binding.process.text.last() == '%' -> false
@@ -31,19 +33,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun ClearAll(view: View) {
+    fun clearAll(view: View) {
         binding.result.text = ""
         binding.process.text = ""
     }
 
     fun delete(view: View) {
-        if(binding.process.text.isNotEmpty()) {
-            binding.process.text = binding.process.text.subSequence(0, binding.process.text.length - 1)
+        if (binding.process.text.isNotEmpty()) {
+            binding.process.text =
+                binding.process.text.subSequence(0, binding.process.text.length - 1)
         }
     }
 
     fun percent(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
             binding.process.append("%")
             canAddDat = true
             canConclude = true
@@ -51,7 +54,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun divide(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
             binding.process.append("÷")
             canAddDat = true
             canConclude = true
@@ -59,7 +62,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun multiplication(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters()){
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
             binding.process.append("x")
             canAddDat = true
             canConclude = true
@@ -67,14 +70,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun minus(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters()){
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
             binding.process.append("-")
             canAddDat = true
             canConclude = true
         }
     }
+
     fun plus(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters()){
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters()) {
             binding.process.append("+")
             canAddDat = true
             canConclude = true
@@ -82,41 +86,67 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun equal(view: View) {
-        if(binding.process.text.isNotEmpty() && controlSpecialCharacters() && canConclude) {
-            val operationList = arrayListOf<String>()
-            val digitsList = arrayListOf<String>()
-            var currentDigit = ""
-            var currentResult = 1
+        if (binding.process.text.isNotEmpty() && controlSpecialCharacters() && canConclude) {
+            val result = calculate()
+            binding.process.text = " "
+            binding.result.text = result
+        }
+    }
 
-            for(character in binding.process.text) {
-                if(character.isDigit() || character == '.') {
-                    currentDigit += character
-                }else {
-                    operationList.add(character.toString())
-                    digitsList.add(currentDigit)
-                    currentDigit = ""
+    private fun calculate(): String {
+        var currentDigit = ""
+        var result = ""
+
+        for (character in binding.process.text) {
+            if (character.isDigit() || character == '.') {
+                currentDigit += character
+            } else {
+                operationList.add(character.toString())
+                digitsList.add(currentDigit)
+                currentDigit = ""
+            }
+        }
+        digitsList.add(currentDigit)
+
+        for (i in 1..operationList.size) {
+            result = if (i == 1) {
+                if (operationList[0] == "+" || operationList[0] == "-") {
+                    additionSubstraction(digitsList[0], digitsList[1], operationList[0])
+                } else {
+                    timesDivison(digitsList[0], digitsList[1], operationList[0])
+                }
+            } else {
+                if (operationList[i - 1] == "+" || operationList[i - 1] == "-") {
+                    additionSubstraction(result, digitsList[i], operationList[i - 1])
+                } else {
+                    timesDivison(result, digitsList[i], operationList[i - 1])
                 }
             }
-            digitsList.add(currentDigit)
+        }
+        operationList.clear()
+        digitsList.clear()
 
-            /*
-                        for(i in 1..operationList.size) {
-                if(binding.process.text.contains("x")) {
-                    if(i == 1) {
-                        currentResult = digitsList[operationList.indexOf("x")].toInt() * digitsList[operationList.indexOf("x") + i].toInt()
-                        Log.e("s", currentResult.toString())
-                        Log.e("s", operationList.size.toString())
-                        Log.e("q", digitsList[operationList.indexOf("x")])
-                        Log.e("q", digitsList[operationList.indexOf("x") + i])
-                    }
-                }
-            }
-             */
+        return result
+    }
+
+    private fun additionSubstraction(s1: String, s2: String, operator: String): String {
+        return if (operator == "+") {
+            (s1.toDouble() + s2.toDouble()).toString()
+        } else {
+            (s1.toDouble() - s2.toDouble()).toString()
+        }
+    }
+
+    private fun timesDivison(s1: String, s2: String, operator: String): String {
+        return if (operator == "x") {
+            (s1.toDouble() * s2.toDouble()).toString()
+        } else {
+            (s1.toDouble() / s2.toDouble()).toString()
         }
     }
 
     fun number(view: View) {
-        if(view is Button) {
+        if (view is Button) {
             when (view.text) {
                 "9" -> binding.process.append("9")
                 "8" -> binding.process.append("8")
@@ -129,8 +159,8 @@ class MainActivity : AppCompatActivity() {
                 "1" -> binding.process.append("1")
                 "0" -> binding.process.append("0")
                 "." -> {
-                    if(binding.process.text.isNotEmpty() && canAddDat && controlSpecialCharacters()) {
-                        if(binding.process.text.last() != '.') {
+                    if (binding.process.text.isNotEmpty() && canAddDat && controlSpecialCharacters()) {
+                        if (binding.process.text.last() != '.') {
                             binding.process.append(".")
                             canAddDat = false
                         }
@@ -140,3 +170,4 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
